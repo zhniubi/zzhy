c3e4a8194f910924be82efaed9529a4fc2bc9bac
zhniubi <1974296745@qq.com>
2019-05-29 11:48:05 +0800

    111

---
+++ config-server/src/main/java/com/jk/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer//开启配置服务器的功能
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
